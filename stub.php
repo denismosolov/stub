a80232a8c38da999651fafa1e9bc58b516bfc73c
denismosolov <denismosolov@gmail.com>
2015-10-31 00:10:13 +0300

    Now it's a daemon which awaits a termination signal

--- stub.php
+++ stub.php
@@ -1,16 +1,37 @@
 <?php
 
-// The idea comes from http://php.net/manual/en/function.stream-socket-server.php
-$socket = stream_socket_server('tcp://0.0.0.0:8000', $errno, $errstr);
-if ($socket) {
-  while ($conn = stream_socket_accept($socket)) {
-    fwrite($conn,     "HTTP/1.1 200 OK \r\n"
-                    . "Content-Type: text/html; charset=utf-8r\n"
-                    . "\r\n"
-                    . "<h1>Stub Web Server</h1><p>Happy learning!</p>");
-    fclose($conn);
-  }
-  fclose($socket);
+$server_is_running = 1;
+
+function sig_handler($signo)
+{
+	global $server_is_running;
+    switch ($signo) {
+        case SIGTERM:
+            // Handle shutdown signal
+            $server_is_running = 0;
+            exit;
+            break;
+        default:
+            // Handle all other signals
+    }
+}
+
+$pid = pcntl_fork();
+if ($pid < 0) {
+    die("Could not fork\n");
+} else if ($pid) {
+    // This is a parent process
+    exit;
 } else {
-  echo "$errstr ($errno)";
+    // Make the child process a session leader
+    if (posix_setsid() < 0) {
+        die("Could not detach from terminal\n");
+    }
+
+    // Setup signal handlers
+    declare(ticks = 1);
+    pcntl_signal(SIGTERM, 'sig_handler');
+
+    // loop until a terminate signal
+    while ($server_is_running);
 }
